34034a4c80e9208e35a663683a97d5fb62c15acc
Tomer <bboypsyfly@gmail.com>
2021-01-11 21:25:03 +0200

    View is ready + method stubs for it's controllers

--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,7 @@
 module PTM2_MS5 {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires javafx.graphics;
 	
 	opens view to javafx.graphics, javafx.fxml;
 }
---
+++ src/view/MainWindowController.java
@@ -0,0 +1,71 @@
+package view;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class MainWindowController implements Observer {
+	
+	// Constructors
+	public MainWindowController() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	// Methods
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void connect() {
+		System.out.println("Connect test");
+	}
+	
+	public void loadData() {
+		
+	}
+	
+	public void calculate() {
+		
+	}
+	
+	public void loadTextFile() {
+		
+	}
+	
+	public void toggleAutoPilot() {
+		
+	}
+	
+	public void runCodeCommands() {
+		
+	}
+	
+	public void toggleManualPilot() {
+		
+	}
+	
+	public void onRudderDrag() {
+		
+	}
+	
+	public void onThrottleDrag() {
+		
+	}
+	
+	public void onRudderRelease() {
+		
+	}
+	
+	public void onThrottleRelease() {
+		
+	}
+	
+	public void onJoystickRelease() {
+		
+	}
+	
+	public void onJoystickDrag() {
+		
+	}
+}
