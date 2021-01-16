4d1bd86558700eee68f47b6c06cd072489b0cb15
Tomer <bboypsyfly@gmail.com>
2021-01-16 15:22:39 +0200

    gitignore change

--- src/module-info.java
+++ src/module-info.java
@@ -2,10 +2,9 @@ module PTM2_MS5 {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires javafx.graphics;
-	requires interpreter;
 	requires javafx.base;
-	requires PTM.projectTest;
 	requires java.desktop;
+	requires interpreter;
 	
 	opens view to javafx.graphics, javafx.fxml;
 }
